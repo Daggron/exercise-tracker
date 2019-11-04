0c44a635308130589575354fc08eef2136bde10d
Daggron <returnofking04@gmail.com>
2019-11-04 20:55:09 +0530

    added functionality to create user component

--- src/components/createuser.components.jsx
+++ src/components/createuser.components.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.changeUsername = this.changeUsername.bind(this);
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+
+        this.state={
+            username:''
+        }
+    }
+
+    changeUsername(e){
+        this.setState({
+            username:e.target.value
+        })
+    }
+
+    onSubmitForm(e){
+        e.preventDefault();
+        const user ={
+            username:this.state.username
+        }
+        console.log(user);
+
+        axios.post('http://localhost:5000/users/add',user)
+        .then(res => console.log(res.data));
+
+        this.setState({
+            username:''
+        });
+    }
+
     render(){
         return(
             <div>
-                <p>
-                    You are on craeteUser Component
-                </p>
+               <form onSubmit={this.onSubmitForm}>
+                    <div className="form-group"> 
+                        <label>Username: </label>
+                        <input  type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={this.changeUsername.bind(this)}
+                            />
+                     </div>
+                     <div className="form-group">
+                         <button className="btn btn-primary">
+                             Submit
+                         </button>
+                     </div>
+               </form>
             </div>
         )
     }
